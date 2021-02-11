f25457568d3c07c8b16849d58bf4d009cc614689
Gal Berger <galbac@gmail.com>
2021-02-12 00:54:38 +0200

    [temp-conv] create main choise loop

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Please choose convertion:");
+        println!("1. Celsius to Fahrenheit");
+        println!("2. Fahrenheit to Celsius");
+
+        let mut user_choise = String::new();
+
+        io::stdin()
+            .read_line(&mut user_choise)
+            .expect("Failed to read line");
+
+        match user_choise.trim().parse() {
+            Ok(num) => match num {
+                1 => {
+                    println!("Converting Celsius to Fahrenheit");
+                    break;
+                },
+                2 => {
+                    println!("Converting Fahrenheit to Celsius");
+                    break;
+                },
+                _ =>  {
+                    println!("Invalid conversion selected.");
+                    continue;
+                }
+            },
+            Err(_) => {
+                println!("Please input only the number of the choise");
+                continue;
+            },
+        };
+    }
+
 }
