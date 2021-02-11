f52a72ada13e211e6b6891c29247aa1d58f72140
Gal Berger <galbac@gmail.com>
2021-02-12 01:38:40 +0200

    [fibonacci] add input function and fibonacci function

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+use std::num::ParseIntError;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Generating the nth Fibonacci number");
+    match get_n() {
+        Ok(num) => println!("The {}th Fibonacci number is: {}", num, fib(num)),
+        Err(_) => println!("Goodbye!"),
+    }
+}
+
+fn get_n() -> Result<u32, ParseIntError> {
+    println!("What is your n today?");
+
+    let mut user_n = String::new();
+
+    io::stdin()
+        .read_line(&mut user_n)
+        .expect("Failed to read line");
+
+    user_n.trim().parse()
+}
+
+fn fib(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        num => fib(num - 1) + fib(num - 2),
+    }
 }
