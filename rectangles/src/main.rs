c463d2666f5f60d761d4c22123e54e84a713c432
Gal Berger <galbac@gmail.com>
2021-02-12 16:38:31 +0200

    [rectangles] implement the Rectangle area method and add debug print formatting

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,6 +1,13 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
@@ -9,12 +16,10 @@ fn main() {
         height: 50,
     };
 
+    println!("rec1 is {:#?}", rec1);
+
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rec1)
+        rec1.area()
     )
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
