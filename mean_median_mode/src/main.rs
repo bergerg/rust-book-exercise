ccbb4cc2e688575b07d92703194a836bccdfee79
Gal Berger <galbac@gmail.com>
2021-02-14 00:26:56 +0200

    [mean_median_mode] add mean function for vector

--- mean_median_mode/src/main.rs
+++ mean_median_mode/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let vec1 = vec![5, 32, 1, 10, 18, 3];
+    assert_eq!(11.5, mean(&vec1), "failed assertion on mean value of vec1");
+
+}
+
+fn mean(v: &Vec<i32>) -> f32 {
+    match v.len() {
+        0 => 0.0,
+        length => v.iter().sum::<i32>() as f32 / length as f32
+    }
 }
