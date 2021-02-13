3797eb75901800dc0b315f7f20ad5b42ac36eaaa
Gal Berger <galbac@gmail.com>
2021-02-14 00:45:28 +0200

    [mean_median_mode] add median function for vector

--- mean_median_mode/src/main.rs
+++ mean_median_mode/src/main.rs
@@ -1,12 +1,23 @@
 fn main() {
     let vec1 = vec![5, 32, 1, 10, 18, 3];
     assert_eq!(11.5, mean(&vec1), "failed assertion on mean value of vec1");
-
+    assert_eq!(10, median(&vec1), "failed assertion on median value of vec1")
 }
 
-fn mean(v: &Vec<i32>) -> f32 {
+fn mean(v: &[i32]) -> f32 {
     match v.len() {
         0 => 0.0,
         length => v.iter().sum::<i32>() as f32 / length as f32
+    }
+}
+
+fn median(v: &[i32]) -> i32 {
+    match v.len() {
+        0 => 0,
+        length => {
+            let mut copy = v.to_vec();
+            copy.sort();
+            copy[length / 2]
+        }
     }
 }
