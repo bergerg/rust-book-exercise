1de75265ce9d3d0435e900a235ea623f05e59cd7
Gal Berger <galbac@gmail.com>
2021-02-14 02:08:05 +0200

    [pig_latin] add piggify and is_vowel functions

--- pig_latin/src/main.rs
+++ pig_latin/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let first = String::from("first");
+    assert_eq!("irst-fay", piggify(&first));
+    let apple = String::from("apple");
+    assert_eq!("apple-hay", piggify(&apple));
+    let hay = String::from("hay");
+    assert_eq!("ay-hay", piggify(&hay));
+    let iter = String::from("iter");
+    assert_eq!("iter-hay", piggify(&iter));
+}
+
+fn piggify(word: &String) -> String {
+    let mut chars = word.chars();
+    match chars.nth(0) {
+        Some(c) if is_vowel(c) => format!("{}-hay", word),
+        Some(c) => format!("{}-{}ay", chars.collect::<String>(), c),
+        None => "".to_string(),
+    }
+}
+
+fn is_vowel(c: char) -> bool {
+    c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
 }
