51397cd13fbc7067414a400c6670b97d88239aeb
Gal Berger <galbac@gmail.com>
2021-02-18 21:25:33 +0200

    [hr_terminal] implement department and person creation according to user input

--- hr_terminal/src/main.rs
+++ hr_terminal/src/main.rs
@@ -8,12 +8,18 @@ fn main() {
     println!("Welcome to Rust HR Terminal!");
     println!("");
 
-    let mut departments: HashMap<String, String> = HashMap::new();
+    let mut departments: HashMap<String, Department> = HashMap::new();
 
     loop {
         match hr_term() {
-            TermialInput::Hire => {},
-            TermialInput::CreateDepartment => {},
+            TermialInput::Hire => {
+                let person: Person = Person::create();
+                // todo: link this person to a department
+            },
+            TermialInput::CreateDepartment => {
+                let department: Department = Department::create();
+                departments.insert(String::from(&department.name), department);
+            },
             TermialInput::ShowDepartments => {},
             TermialInput::Exit => break,
         }
