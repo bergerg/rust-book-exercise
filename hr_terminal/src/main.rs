ed92b86434fd0c0ae93130acff3575f30ba1db3e
Gal Berger <galbac@gmail.com>
2021-02-18 21:16:32 +0200

    [hr_terminal] use main input in main function

--- hr_terminal/src/main.rs
+++ hr_terminal/src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
+mod terminal;
+
+use crate::terminal::hr::{hr_term, TermialInput};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Rust HR Terminal!");
+    println!("");
+
+    let mut departments: HashMap<String, String> = HashMap::new();
+
+    loop {
+        match hr_term() {
+            TermialInput::Hire => {},
+            TermialInput::CreateDepartment => {},
+            TermialInput::ShowDepartments => {},
+            TermialInput::Exit => break,
+        }
+
+    }
+    println!("Goodbye!");
 }
