ffc82d3b0ee40dadd54bb02e9f4007658a65f2cd
Gal Berger <galbac@gmail.com>
2021-02-18 21:22:53 +0200

    [hr_terminal] add associated functions to Department

--- hr_terminal/src/terminal/departments.rs
+++ hr_terminal/src/terminal/departments.rs
@@ -1,7 +1,17 @@
+use super::common::input;
 use crate::terminal::personal::Person;
 
 #[derive(Debug)]
 pub struct Department {
     pub name: String,
     personal: Vec<Person>
+}
+
+impl Department {
+    pub fn create() -> Department {
+        Department {
+            name: input("Please enter a name for the new department"),
+            personal: vec![],
+        }
+    }
 }
