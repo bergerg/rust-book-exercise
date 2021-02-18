f80c7477a38f3d2b675a23ea3623bc2e4e55d1cb
Gal Berger <galbac@gmail.com>
2021-02-18 21:22:13 +0200

    [hr_terminal] add associated functions to Person

--- hr_terminal/src/terminal/personal.rs
+++ hr_terminal/src/terminal/personal.rs
@@ -1,6 +1,27 @@
+use super::common::input;
+
 #[derive(Debug)]
 pub struct Person {
     first_name: String,
     last_name: String,
     age: u8
 }
+
+impl Person {
+    pub fn create() -> Person {
+        Person {
+            first_name: input("Please enter first name"),
+            last_name: input("Please enter last name"),
+            age: input("Please enter age").trim().parse().expect("Age must be a number"),
+        }
+    }
+
+    pub fn from(person: &Person) -> Person {
+        Person {
+            first_name: String::from(&person.first_name),
+            last_name: String::from(&person.last_name),
+            age: person.age
+        }
+    }
+}
+
